5ae23ff0d1d36f4f1fccaa2239b18a9213f138aa
brandon-charest <b.charest019@gmail.com>
2022-08-25 20:41:29 -0400

    telescope update

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -107,6 +107,7 @@ return packer.startup(function(use)
     run = ":TSUpdate",
   }
   use 'JoosepAlviste/nvim-ts-context-commentstring'
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
   -- Git
   use "lewis6991/gitsigns.nvim"
