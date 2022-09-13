2e214c7589b7f7b3fcac5ca77f641a18855cddf4
brandon-charest <b.charest019@gmail.com>
2022-09-12 20:07:50 -0400

    dashboard

--- .config/nvim/lua/user/alpha.lua
+++ .config/nvim/lua/user/alpha.lua
@@ -1,44 +1,59 @@
 local status_ok, alpha = pcall(require, "alpha")
 if not status_ok then
-	return
+  return
+end
+
+math.randomseed(os.time())
+
+local function pick_color()
+    local colors = {"String", "Identifier", "Keyword", "Number"}
+    return colors[math.random(#colors)]
 end
 
 local dashboard = require("alpha.themes.dashboard")
-dashboard.section.header.val = {
-	[[                               __                ]],
-	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+local logo = {
+  "                                                     ",
+  "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+  "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+  "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+  "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+  "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+  "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+  "                                                     ",
 }
+
+dashboard.section.header.val = logo
+dashboard.section.header.opts.hl = pick_color()
+
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+  dashboard.button("f", "  find file", ":Telescope find_files <cr>"),
+  dashboard.button("e", "  new file", ":ene <bar> startinsert <cr>"),
+  dashboard.button("p", "  find project", ":Telescope projects <cr>"),
+  dashboard.button("r", "  recently used files", ":Telescope oldfiles <cr>"),
+  dashboard.button("t", "  find text", ":Telescope live_grep <cr>"),
+  dashboard.button("c", "  configuration", ":e ~/.config/nvim/init.lua <cr>"),
+  dashboard.button("q", "  quit neovim", ":qa<cr>"),
 }
 
 local function footer()
-  local date = os.date("  %d/%m/%Y ")
-  local time = os.date("  %H:%M:%S ")
-  -- local plugins = "  " .. #vim.tbl_keys(packer_plugins) .. " plugins "
+  -- local date = os.date("  %d/%m/%y ")
+  -- local time = os.date("  %h:%m:%s ")
+  local datetime = os.date(" %d-%m-%Y   %H:%M:%S")
+  --  local plugins = "  " .. #vim.tbl_keys(packer_plugins) .. " plugins "
+  local plugins = "   " ..
+      vim.fn.len(vim.fn.globpath("~/.local/share/nvim/site/pack/packer/start", "*", 0, 1)) .. " plugins "
 
   local v = vim.version()
   local version = "  v" .. v.major .. "." .. v.minor .. "." .. v.patch
 
-  return date .. time ..  version
+  return datetime .. plugins .. version
 end
 
 dashboard.section.footer.val = footer()
+dashboard.section.footer.opts.hl = "Constant"
 
-dashboard.section.footer.opts.hl = "Type"
-dashboard.section.header.opts.hl = "Include"
-dashboard.section.buttons.opts.hl = "Keyword"
+dashboard.section.buttons.opts.hl = "keyword"
 
-dashboard.opts.opts.noautocmd = true
--- vim.cmd([[autocmd User AlphaReady echo 'ready']])
 alpha.setup(dashboard.opts)
+
+vim.cmd([[ autocmd FileType alpha setlocal nofoldenable ]])
