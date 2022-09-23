3025c2f6e952e7940fc020c8c178b035cd5da577
brandon-charest <b.charest019@gmail.com>
2022-09-23 19:53:18 -0400

    mason and typescript

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -82,6 +82,8 @@ return packer.startup(function(use)
     requires = { 'kyazdani42/nvim-web-devicons' }
   }
   use "antoinemadec/FixCursorHold.nvim" -- This is needed to fix lsp doc highlight
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
 
   -- Languages
   use "jalvesaq/Nvim-R" -- R language
@@ -109,9 +111,10 @@ return packer.startup(function(use)
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  -- use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "tamago324/nlsp-settings.nvim" -- language server settings defined in json for
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
+  use "https://git.sr.ht/~whynothugo/lsp_lines.nvim" -- virtual lines
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
